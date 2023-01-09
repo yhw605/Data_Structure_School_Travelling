13390846f58a2748e409980c03c1f25e46dcce80
yhw605 <118654633+yhw605@users.noreply.github.com>
2023-01-09 09:31:19 +0800

    Delete maxFlow.h

--- maxFlow.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAX_FLOW_H
-#define _MAX_FLOW_H
-#include "queues.h"
-
-void fordFulkerson(Graph* g, vexNode* v, int start, int end);
-
-int bfs(int start, int end, int vexNum);
-
-#endif // !_MAX_FLOW_H
